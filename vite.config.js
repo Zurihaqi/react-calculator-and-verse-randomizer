8cd9a5bd29b2b5ef51a62416eb876d63c0bdfa0f
Zul Fahri Baihaqi <zurihaqi@gmail.com>
2024-08-25 18:18:05 +0700

    vercel.json fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
+import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
