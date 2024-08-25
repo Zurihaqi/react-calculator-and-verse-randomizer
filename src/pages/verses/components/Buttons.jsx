e002055f6571ea6bf8a7dc955e70cd47a8ae78f7
Zul Fahri Baihaqi <zurihaqi@gmail.com>
2024-08-25 19:16:08 +0700

    change ayah to ayat

--- src/pages/verses/components/Buttons.jsx
+++ src/pages/verses/components/Buttons.jsx
@@ -19,7 +19,7 @@ export default class Buttons extends Component {
           className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white mt-4"
         >
           <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">
-            Previous Ayah
+            Previous Ayat
           </span>
         </button>
         <button
@@ -50,7 +50,7 @@ export default class Buttons extends Component {
           className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white mt-4"
         >
           <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">
-            Next Ayah
+            Next Ayat
           </span>
         </button>
       </div>
