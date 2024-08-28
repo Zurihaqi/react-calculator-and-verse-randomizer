37120385cb1eaf0b8b9f165765c9735de2606d68
Zul Fahri Baihaqi <zurihaqi@gmail.com>
2024-08-28 09:15:39 +0700

    bug fix

--- src/pages/verses/components/ButtonGroup.jsx
+++ src/pages/verses/components/ButtonGroup.jsx
@@ -17,7 +17,7 @@ export default class ButtonGroup extends Component {
   }
 
   ayahsPerSurah = [
-    0, 7, 286, 200, 176, 120, 165, 206, 75, 129, 109, 123, 111, 43, 52, 99, 128,
+    7, 286, 200, 176, 120, 165, 206, 75, 129, 109, 123, 111, 43, 52, 99, 128,
     111, 110, 98, 135, 112, 78, 118, 64, 77, 227, 93, 88, 69, 60, 34, 30, 73,
     54, 45, 83, 182, 88, 75, 85, 54, 53, 89, 59, 37, 35, 38, 29, 18, 45, 60, 49,
     62, 55, 78, 96, 29, 22, 24, 13, 14, 11, 11, 18, 12, 12, 30, 52, 52, 44, 28,
@@ -68,7 +68,7 @@ export default class ButtonGroup extends Component {
       if (randomizer === 1) {
         newSurahNumber = surahNumber - 1;
         await fetchSurah(newSurahNumber);
-        newRandomizer = this.ayahsPerSurah[surahNumber - 1];
+        newRandomizer = this.ayahsPerSurah[surahNumber - 2];
       } else {
         newRandomizer = randomizer - 1;
       }
