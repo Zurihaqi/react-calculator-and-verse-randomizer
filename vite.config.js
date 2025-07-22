9dbaf18e7b424a8bdd56166e94d19c5665d16c7e
Zul Fahri Baihaqi <zurihaqi@gmail.com>
2025-07-22 14:12:41 +0700

    resolve deps issues

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import vercel from 'vite-plugin-vercel';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 
 export default defineConfig({
-  plugins: [react(), vercel()],
+  plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
-  }
-})  
+  },
+});
